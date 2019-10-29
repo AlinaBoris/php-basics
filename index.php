b0521f5b1e6e28042f0683be5e92415b05f1b9b8
AlinaBoris <alinbori949@gmail.com>
2019-10-29 15:05:31 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -1,1 +1,12 @@
-<?php print 'veikia pagaliau';
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title>As valdau HTML</title>
+  </head>
+  <body>
+    <title>As valdau HTML</title>
+    <h1>Alina - HTML ase!</h1>
+    <p>Moku kurti statini turini.</p>
+  </body>
+</html>
