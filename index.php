7759a9c498a55034813ed1c3ffa784c2b941d18c
AlinaBoris <alinbori949@gmail.com>
2019-10-29 17:16:42 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title><?php print 'Aš, '. date('l'). ' ir PHP' ?>!</title>
+    <title>PHP lydės <?php print date ('Y-m-d', strtotime('+1 day')); ?>!</title>
   </head>
   <body>
-    <h1><b>Alina</b> - HTML <?php print 'ir PHP '; ?> asė jau nuo <?php print date('Y'). ' metų' ?>!</h1>
+    <h1><b>Alina</b> - PHP su manim buvo ir <?php print date ('H', strtotime('-1 hour')) . ' valandą'; ?>!</h1>
     <p>
-        Viskas prasidėjo <?php print date('m') . ' mėnesio, ' . date('d') . ' dieną!'; ?>
+        <?php print date ('Y', strtotime('+1 year')); ?> ne už kalnų!
     </p>
   </body>
 </html>
