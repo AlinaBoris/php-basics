51766c54d0d32b9ff4ec06c12c68c9b08b817f1a
AlinaBoris <alinbori949@gmail.com>
2019-10-29 15:49:38 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -1,12 +1,12 @@
-<!DOCTYPE html>
+
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>As valdau HTML</title>
+    <title>Aš valdau HTML <?php print 'ir PHP' ?>!</title>
   </head>
   <body>
-    <title>As valdau HTML</title>
-    <h1>Alina - HTML ase!</h1>
-    <p>Moku kurti statini turini.</p>
+    <h1><b>Alina</b> - HTML <?php print 'ir PHP' ?> asė!</h1>
+    <p>Moku kurti statinį turinį.<br>
+        Atydaryta: <?php print date('Y-m-d H:i:s') ?></p>
   </body>
 </html>
