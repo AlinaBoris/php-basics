a70bb6eb834bfbdb69800af122fd60accc7263f8
AlinaBoris <alinbori949@gmail.com>
2019-10-31 17:55:34 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -4,12 +4,20 @@
     $price = 1.3;
     $fuel_total = round($distance * $consumption / 100, 2);
     $price_trip = round($fuel_total * $price, 2);
+    $my_money = 100;
     
+    if ($fuel_total <= $my_money) {
+        $lottery = 'įperkama.'; 
+    }   else {
+        $lottery = 'neįperkama';
+    }
     
     $h1 = 'Kelionės skaičiuoklė';
     $li_1 = "Nuvažiouta distancija: $distance.";
     $li_2 = "Sunaudota $fuel_total l. kuro.";
     $li_3 = "Kaina: $price_trip pinigų.";
+    $li_4 = "Turimi pinigai: $my_money.";
+    $p = "Išvada: Kelione pr $lottery";  
 ?>
 <html lang="en" dir="ltr">
   <head>
@@ -19,9 +27,12 @@
   <body>
       <h1><?php print $h1;?></h1>
       <ul>
-          <li><?php print $li_1;?></li>
-          <li><?php print $li_2;?></li>
-          <li><?php print $li_3;?></li>
+        <li><?php print $li_1;?></li>
+        <li><?php print $li_2;?></li>
+        <li><?php print $li_3;?></li>
+        <li><?php print $li_4;?></li>
       </ul>
+      <hr>
+      <p><?php print $p;?></p>
   </body>
 </html>
