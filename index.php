36fe621e5df3ab1a8c69b18b7ccab4f7d196e017
AlinaBoris <alinbori949@gmail.com>
2019-10-31 16:40:48 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -1,14 +1,13 @@
 <?php 
-    $rand_1_100 = rand(1, 100);
-    $rand_101_200 = rand(101, 200);
-    $rand_201_300 = rand(201, 300);
-    $rand_301_400 = rand(301, 400);
+    $bin_vol = 40;
+    $bin_heap_vol = rand(5, 40);
+    $trash_per_day = 15;
+    $days = floor(($bin_vol + $bin_heap_vol)/$trash_per_day);
     
-    $h1 = "Skolos skaičiuoklė";
-    $h2 = "Jeigu paimei $rand_1_100 euru";
-    $h3 = "Su diem kabančiais grąžinsi $rand_201_300";
-    $h4 = "Su vienu kabančiu grąžinsi $rand_101_200";
-            
+    $h1 = 'Šiukšlių prognozė';
+    $p_1 = "Turima šiukšlinė - $bin_vol litrų.";
+    $p_2 = "Žmona nieko nesako, kol kaupas neviršija $bin_heap_vol litrų.";
+    $h3 = "Išvada: Nieko nedarysiu $days dienų.";
 ?>
 <html lang="en" dir="ltr">
   <head>
@@ -17,8 +16,8 @@
   </head>
   <body>
       <h1><?php print $h1?></h1>
-      <h2><?php print $h2?></h2>
-      <h3><?php print $h3?></h3>
-      <h4><?php print $h4?></h4>
+      <p><?php print $p_1?></p>
+      <p><?php print $p_2?></p>
+      <p><?php print $h3?></p>
   </body>
 </html>
