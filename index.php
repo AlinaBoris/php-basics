f7a9fc30a0260e5a9f07ef43007a810b9067ff16
AlinaBoris <alinbori949@gmail.com>
2019-11-04 15:12:38 +0200

    Praktika su ..

--- index.php
+++ index.php
@@ -1,38 +1,26 @@
 <?php 
-    $distance = rand(5, 350);
-    $consumption = 7.5;
-    $price = 1.3;
-    $fuel_total = round($distance * $consumption / 100, 2);
-    $price_trip = round($fuel_total * $price, 2);
-    $my_money = 100;
+    $grizai_velai - rand(0, 1);
+    $grizai_isgeres - rand(0, 1);
     
-    if ($fuel_total <= $my_money) {
-        $lottery = 'įperkama.'; 
-    }   else {
-        $lottery = 'neįperkama';
+    if ($grizai_velai) {
+        $situacija = 'Grįžai vėlai.'; 
+    }   elseif ($grizai_velai && $grizai_isgeres) { 
+        $situacija = 'Grįžai vėlai ir išgėręs.';
+    }   elseif ($grizai_isgeres) { 
+        $situacija = 'Grįžai išgėręs.';
+    }   else { 
+        $situacija = 'Nieko nepadarei.';
     }
     
-    $h1 = 'Kelionės skaičiuoklė';
-    $li_1 = "Nuvažiouta distancija: $distance.";
-    $li_2 = "Sunaudota $fuel_total l. kuro.";
-    $li_3 = "Kaina: $price_trip pinigų.";
-    $li_4 = "Turimi pinigai: $my_money.";
-    $p = "Išvada: Kelione pr $lottery";  
+    $h1 = 'Biutinė skaičiuoklė';
 ?>
 <html lang="en" dir="ltr">
   <head>
     <meta charset="utf-8">
-    <title>Task</title>
+    <title>Situacija</title>
   </head>
   <body>
       <h1><?php print $h1;?></h1>
-      <ul>
-        <li><?php print $li_1;?></li>
-        <li><?php print $li_2;?></li>
-        <li><?php print $li_3;?></li>
-        <li><?php print $li_4;?></li>
-      </ul>
-      <hr>
-      <p><?php print $p;?></p>
+      <h2><?php print $situacija;?></h2>
   </body>
 </html>
