019d4636b52477053c3073624fd9b33c93c202f5
AlinaBoris <alinbori949@gmail.com>
2019-11-07 16:05:33 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -1,40 +1,40 @@
 <?php
 $product = [
-    'top_layer' => [
+    [
         'products'=> [
-            1 => [
-                    'name' => 'Kiausiniai',
-                    'quantity' => 3,
+            [
+                'name' => 'Kiausiniai',
+                'quantity' => 3,
             ],
-            2 => [
-                    'name' => 'Grietine',
-                    'quantity' => 1,
+            [
+                'name' => 'Grietine',
+                'quantity' => 1,
             ],
-            3 => [
-                    'name' => 'Jogurtas',
-                    'quantity' => 1,
+            [
+                'name' => 'Jogurtas',
+                'quantity' => 1,
             ],
-            4 => [
-                    'name' => 'Zuvis',
-                    'quantity' => 0,
+            [
+                'name' => 'Zuvis',
+                'quantity' => 0,
             ]
         ]
     ],
-    'bottom_layer' => [
+    [
         'products'=> [
-            1 => [
+            [
                 'name' => 'Pienas',
                 'quantity' => 1,
             ],
-            2 => [
+            [
                 'name' => 'Kefyras',
                 'quantity' => 0,
             ],
-            3 => [
+            [
                 'name' => 'Degtine',
                 'quantity' => 2,
             ],
-            4 => [
+            [
                 'name' => 'Keciupas',
                 'quantity' => 2,
             ]
