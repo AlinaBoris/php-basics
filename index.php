b82cb2e8c1d698c95a2f5ce2695c02dbff4a18b7
AlinaBoris <alinbori949@gmail.com>
2019-11-07 16:01:43 +0200

    Praktika su ...

--- index.php
+++ index.php
@@ -1,41 +1,45 @@
 <?php
 $product = [
-    [
-        'user' => [
-            'name'=> 'Petro',
-            'surname' => 'Pizdzio',
-        ],
-        'act' => [
-            1 => 'prap*stas',
-            'items' => [
-                1 => 'telefonas'
+    'top_layer' => [
+        'products'=> [
+            1 => [
+                    'name' => 'Kiausiniai',
+                    'quantity' => 3,
+            ],
+            2 => [
+                    'name' => 'Grietine',
+                    'quantity' => 1,
+            ],
+            3 => [
+                    'name' => 'Jogurtas',
+                    'quantity' => 1,
+            ],
+            4 => [
+                    'name' => 'Zuvis',
+                    'quantity' => 0,
             ]
+        ]
     ],
-    ],
-    [
-        'user' => [
-            'name'=> 'Tomo',
-            'surname' => 'Ablomo',
-        ],
-        'act' => [
-            1 => 'naudojamas',
-            'items' => [
-                1 => 'buljonas'
+    'bottom_layer' => [
+        'products'=> [
+            1 => [
+                'name' => 'Pienas',
+                'quantity' => 1,
+            ],
+            2 => [
+                'name' => 'Kefyras',
+                'quantity' => 0,
+            ],
+            3 => [
+                'name' => 'Degtine',
+                'quantity' => 2,
+            ],
+            4 => [
+                'name' => 'Keciupas',
+                'quantity' => 2,
             ]
-    ],
-    ],
-    [
-        'user' => [
-            'name'=> 'Anos',
-            'surname' => 'Shitkova',
-        ],
-        'act' => [
-            1 => 'pasibaiges',
-            'items' => [
-                1 => 'rulonas'
-            ]
-    ]
-    ],
-    ];
+        ]
+    ]   
+]; 
 
 var_dump($product);
